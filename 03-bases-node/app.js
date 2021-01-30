e06f10c8588950ab43e43f531f08468ba8826087
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-30 18:48:31 -0200

    Inicio del proyecto

---
+++ 03-bases-node/app.js
@@ -0,0 +1,11 @@
+console.clear();
+console.log('=======================');
+console.log('     Tabla del: 5');
+console.log('=======================');
+
+const base = 5;
+
+for (let i = 0; i <= 10; i++) {
+
+     console.log(`${ base } X ${ i } = ${ base * i }`);
+}
