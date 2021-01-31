76a17f776e6b609683d691882d74e24a86aeb727
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-31 18:29:45 -0200

    Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,16 +1,23 @@
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
 
 console.clear();
 
-const [ , , arg3 = 'base=5' ] = process.argv;
-const [ , base = 5 ] = arg3.split('=');
+console.log( process.argv );
+console.log( argv );
+
+console.log('base: yargs', argv.base);
+
+
+// const [ , , arg3 = 'base=5' ] = process.argv;
+// const [ , base = 5 ] = arg3.split('=');
 // console.log( base );
 
 // const base = 3;
 
-crearArchivo( base )
-     .then( nombreArchivo => console.log( nombreArchivo, 'creado' ) )
-     .catch( err => console.log(err) );
+// crearArchivo( base )
+//      .then( nombreArchivo => console.log( nombreArchivo, 'creado' ) )
+//      .catch( err => console.log(err) );
 
 // fs.writeFile( `tabla-${ base }.txt`, salida, (err) => {
 //      if (err) throw err;
