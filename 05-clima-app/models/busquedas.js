f263c28ff41376f756f65daf04f23b4921d2c2cd
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-28 00:41:16 -0300

    Modelo para controlar la aplicacion

---
+++ 05-clima-app/models/busquedas.js
@@ -0,0 +1,23 @@
+
+
+
+class Busquedas {
+
+     historial = ['Tegucigalpa', 'Madrid', 'San josé'];
+
+     constructor() {
+          // TODO DB si existe
+     }
+
+     async ciudad( lugar = '' ) {
+
+          // petición http
+          console.log(lugar);
+
+
+          return []; // retornar los lugares
+     }
+
+}
+
+module.exports = Busquedas;
