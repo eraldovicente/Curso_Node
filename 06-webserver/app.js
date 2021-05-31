2c2a24bf1fb8a601721a43b200e9b5aa53fa74de
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-31 15:39:14 -0300

    Desplegando aplicaciones de Angular y React

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -12,30 +12,30 @@ hbs.registerPartials( __dirname + '/views/partials', function (err) {});
 // Servir contenido estático
 app.use( express.static('public') );
  
-app.get('/', (req, res) => {
-     res.render('home', {
-          nombre: 'Fernando Herrera',
-          titulo: 'Curso de Node'
-     });
-});
-
-app.get('/generic', (req, res) => {
-     res.render('generic', {
-          nombre: 'Fernando Herrera',
-          titulo: 'Curso de Node'
-     });
-});
-
-app.get('/elements', (req, res) => {
-     res.render('elements', {
-          nombre: 'Fernando Herrera',
-          titulo: 'Curso de Node'
-     });
-});
-
-// app.get('*', (req, res) => {
-//      res.sendFile( __dirname + '/public/404.html');
+// app.get('/', (req, res) => {
+//      res.render('home', {
+//           nombre: 'Fernando Herrera',
+//           titulo: 'Curso de Node'
+//      });
 // });
+
+// app.get('/generic', (req, res) => {
+//      res.render('generic', {
+//           nombre: 'Fernando Herrera',
+//           titulo: 'Curso de Node'
+//      });
+// });
+
+// app.get('/elements', (req, res) => {
+//      res.render('elements', {
+//           nombre: 'Fernando Herrera',
+//           titulo: 'Curso de Node'
+//      });
+// });
+
+app.get('*', (req, res) => {
+     res.sendFile( __dirname + '/public/index.html');
+});
  
 app.listen(port, () => {
      console.log(`Example app listening at http://localhost:${port}`);
