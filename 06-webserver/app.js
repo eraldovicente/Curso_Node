ef6ceb930b66e6890ed475044447b8acdcfbca74
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-30 21:33:16 -0300

    Request y Response

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -4,7 +4,17 @@ const http = require('http');
 
 http.createServer( ( req, res ) => {
 
-     res.write('Hola Mundo');
+     // res.writeHead(200, { 'Content-Type': 'application/json' });
+     res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+     res.writeHead(200, { 'Content-Type': 'application/csv' });
+
+     res.write( 'id, nombre\n' );
+     res.write( '1, Eraldo\n' );
+     res.write( '2, Edna\n' );
+     res.write( '3, Eraldo\n' );
+     res.write( '4, Eduardo\n' );
+     res.write( '5, Gabriel\n' );
+     res.write( '6, Bábarba\n' );
      res.end();
 
 })
