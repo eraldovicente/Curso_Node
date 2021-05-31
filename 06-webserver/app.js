05501067261cfaaadd34122b1b2a04ab0104d37c
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-30 22:52:27 -0300

    Introduccion a EXPRESS

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,23 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+ 
+app.get('/', (req, res) => {
+     res.send('Home page')
+});
 
+app.get('/hola-mundo', (req, res) => {
+     res.send('Hola mundo en su respectiva ruta')
+});
 
-const http = require('http');
-
-http.createServer( ( req, res ) => {
-
-     // res.writeHead(200, { 'Content-Type': 'application/json' });
-     res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
-     res.writeHead(200, { 'Content-Type': 'application/csv' });
-
-     res.write( 'id, nombre\n' );
-     res.write( '1, Eraldo\n' );
-     res.write( '2, Edna\n' );
-     res.write( '3, Eraldo\n' );
-     res.write( '4, Eduardo\n' );
-     res.write( '5, Gabriel\n' );
-     res.write( '6, Bábarba\n' );
-     res.end();
-
+app.get('*', (req, res) => {
+     res.send('404 | Page not found')
+});
+ 
+app.listen(port, () => {
+     console.log(`Example app listening at http://localhost:${port}`);
 })
-.listen( 8080 );
-
-console.log('Escuchando el puerto', 8080 );
