fb64b4173dc4ab2cdc1281705130532b8c477e68
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-03 00:44:23 -0300

    POST: Creando un usuario en la coleccion

--- 07-restserver/controllers/usuarios.js
+++ 07-restserver/controllers/usuarios.js
@@ -1,5 +1,7 @@
 const { response } = require('express');
 
+const Usuario = require('../models/usuario');
+
 const usuariosGet = ( req = request, res = response ) => {
 
      const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
@@ -14,14 +16,15 @@ const usuariosGet = ( req = request, res = response ) => {
      });
 }
 
-const usuariosPost = (req, res = response ) => {
+const usuariosPost = async(req, res = response ) => {
 
-     const { nombre, edad } = req.body;
+     const body = req.body;
+     const usuario = new Usuario( body );
+
+     await usuario.save();
 
      res.json({
-          msg: 'post API',
-          nombre,
-          edad
+          usuario
      });
 }
 
