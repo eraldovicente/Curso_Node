cf734ea376a622e9af14bd1b49b37e307343642f
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-07 22:42:02 -0300

    Login de usuario

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -1,10 +1,49 @@
-const { response } = require("express")
+const { response } = require("express");
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res = response) => {
+const Usuario = require('../models/usuario');
 
-     res.json({
-          msg: 'Login ok'
-     })
+const login = async(req, res = response) => {
+
+     const { correo, password } = req.body;
+
+     try {
+
+          // Verificar si el email existe
+          const usuario = await Usuario.findOne({ correo });
+          if ( !usuario ) {
+               return res.status(400).json({
+                    msg: 'Usuario / Password no son correctos - correo'
+               });
+          }
+
+          // Si el usuario está activo
+          if ( !usuario.estado ) {
+               return res.status(400).json({
+                    msg: 'Usuario / Password no son correctos - estado: false'
+               });
+          }
+
+          // Verificar la contraseña
+          const validPassword = bcryptjs.compareSync( password, usuario.password );
+          if ( !validPassword ) {
+               return res.status(400).json({
+                    msg: 'Usuario / Password no son correctos - password'
+               });
+          }
+
+          // Generar el JWT
+
+          res.json({
+               msg: 'Login ok'
+          });
+          
+     } catch (error) {
+          console.log(error);
+          return res.status(500).json({
+               msg: 'Hable con el administrador'
+          });
+     }
 }
 
 module.exports = {
