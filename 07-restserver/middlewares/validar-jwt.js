8e3bb1749c1267a72fea7651013376c32ad91cc5
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-08 17:08:44 -0300

    Obtener la informacion del usuario autenticado

--- 07-restserver/middlewares/validar-jwt.js
+++ 07-restserver/middlewares/validar-jwt.js
@@ -1,7 +1,9 @@
 const { request, response } = require('express');
 const jwt = require('jsonwebtoken');
 
-const validarJWT = ( req = request, res = response, next ) => {
+const Usuario = require('../models/usuario');
+
+const validarJWT = async( req = request, res = response, next ) => {
 
      const token = req.header('x-token');
 
@@ -12,11 +14,26 @@ const validarJWT = ( req = request, res = response, next ) => {
      }
 
      try {
-
+ 
           const { uid } = jwt.verify( token, process.env.SECRETORPRIVATEKEY );
 
-          req.uid = uid;
-          
+          // Leer el usuario que corresponde al uid
+          const usuario = await Usuario.findById( uid );
+
+          if ( !usuario ) {
+               return res.status(401).json({
+                    msg: 'Token no válido - usuario no existe en DB'
+               });
+          }
+
+          // Verificar si el uid tiene estado true
+          if ( !usuario.estado ) {
+               return res.status(401).json({
+                    msg: 'Token no válido - usuario con estado: false'
+               });
+          }
+
+          req.usuario = usuario;
           next();
 
      } catch (error) {
