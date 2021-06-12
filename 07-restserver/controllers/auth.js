cd97a0b619fda81734092afad4cc89a9a62266c9
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-11 21:50:21 -0300

    Crear un usuario personalizado con credenciales

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -57,13 +57,37 @@ const googleSignin = async(req, res = response ) => {
 
      try {
 
-          const googleUser = await googleVerify( id_token );
+          const { correo, img, nombre } = await googleVerify( id_token );
 
-          console.log(googleUser);
+          let usuario = await Usuario.findOne({ correo });
+
+          if ( !usuario ) {
+               // Tengo que crearlo
+               const data = {
+                    nombre,
+                    correo,
+                    password: ':P',
+                    img,
+                    google: true
+               };
+
+               usuario = new Usuario( data );
+               await usuario.save();
+          }
+
+          // Si el usuario en DB
+          if ( !usuario.estado ) {
+               return res.status(401).json({
+                    msg: 'Hable con el administrador, usuario bloqueado'
+               });
+          }
+
+          // Generar el JWT
+          const token = await generarJWT( usuario.id );
 
           res.json({
-               msg: 'Todo ok! Google signin',
-               googleUser
+               usuario,
+               token
           });
 
      } catch (error) {
