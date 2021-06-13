09da04410c0bbcec44b593a1f8980f5ce90299fe
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-12 22:14:37 -0300

    Crear una categoria

--- 07-restserver/routes/categorias.js
+++ 07-restserver/routes/categorias.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT, validarCampos } = require('../middlewares');
 
-const { login, googleSignin } = require('../controllers/auth');
+const { crearCategoria } = require('../controllers/categorias');
 
 const router = Router();
 
@@ -22,9 +22,11 @@ router.get('/:id', ( req, res ) => {
 });
 
 // Crear categoria - privado - cualquier persona con un token válido
-router.post('/', ( req, res ) => {
-     res.json('post');
-});
+router.post('/', [ 
+     validarJWT,
+     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+     validarCampos 
+], crearCategoria );
 
 // Actualizar - privado - cualquiera con token válido
 router.put('/:id', ( req, res ) => {
