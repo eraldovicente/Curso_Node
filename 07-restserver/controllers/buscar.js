b2b69c67ee0ee5c951aa213c97423540cd3d4b3b
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-14 22:14:01 -0300

    Buscar en otras colecciones

--- 07-restserver/controllers/buscar.js
+++ 07-restserver/controllers/buscar.js
@@ -33,6 +33,48 @@ const buscarUsuarios = async( termino = '', res = response ) => {
      });
 }
 
+const buscarCategorias = async( termino = '', res = response ) => {
+
+     const esMongoID = ObjectId.isValid( termino ); // True
+
+     if ( esMongoID ) {
+          const categoria = await Categoria.findById( termino );
+          return res.json({
+               results: ( categoria ) ? [ categoria ] : []
+          });
+     }
+
+     const regex = new RegExp( termino, 'i' );
+
+     const categorias = await Categoria.find({ nombre: regex, estado: true });
+
+     res.json({
+          results: categorias
+     });
+}
+
+const buscarProductos = async( termino = '', res = response ) => {
+
+     const esMongoID = ObjectId.isValid( termino ); // True
+
+     if ( esMongoID ) {
+          const producto = await Producto.findById( termino )
+                                   .populate('categoria', 'nombre');
+          return res.json({
+               results: ( producto ) ? [ producto ] : []
+          });
+     }
+
+     const regex = new RegExp( termino, 'i' );
+
+     const productos = await Producto.find({ nombre: regex, estado: true })
+                                   .populate('categoria', 'nombre');
+
+     res.json({
+          results: productos
+     });
+}
+
 const buscar = ( req, res = response ) => {
 
      const { coleccion, termino } = req.params;
@@ -48,8 +90,10 @@ const buscar = ( req, res = response ) => {
                buscarUsuarios(termino, res);
                break;
           case 'categorias':
+               buscarCategorias(termino, res);
                break;
           case 'productos':
+               buscarProductos(termino, res);
                break;
      
           default:
