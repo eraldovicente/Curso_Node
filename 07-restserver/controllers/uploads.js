0de93409d5a6eb3b44d336e10ab86875bde7d7a6
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-16 17:00:46 -0300

    Resolucion de la tarea Desestruturar de undefined

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -5,10 +5,6 @@ const { Usuario, Producto } = require('../models');
 
 const cargarArchivo = async( req, res = response ) => {
 
-     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo ) {
-          res.status(400).json({msg: 'No hay archivos que subir'});
-     }
-
      try {
           
           // txt, md
