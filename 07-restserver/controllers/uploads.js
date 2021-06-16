eef9fd9e130eb3801ced21d20a9e84292ae61dc0
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-16 19:40:53 -0300

    Servicio para mostrar las imagenes

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -71,7 +71,53 @@ const actualizarImagen = async(req, res = response ) => {
 
 }
 
+
+const mostrarImage = async(req, res = response ) => {
+
+     const { id, coleccion } = req.params;
+
+     let modelo;
+
+     switch ( coleccion ) {
+          case 'usuarios':
+               modelo = await Usuario.findById(id);
+               if ( !modelo ) {
+                    return res.status(400).json({
+                         msg: `No existe un usuario con el id ${ id }`
+                    });
+               }
+
+          break;
+
+          case 'productos':
+               modelo = await Producto.findById(id);
+               if ( !modelo ) {
+                    return res.status(400).json({
+                         msg: `No existe un producto con el id ${ id }`
+                    });
+               }
+
+          break;
+     
+          default:
+               return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+     }
+
+     // Limpiar imágenes previas
+     if ( modelo.img ) {
+          // Hay que borrar la imagen del servidor
+          const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
+          if ( fs.existsSync( pathImagen ) ) {
+               return res.sendFile( pathImagen );
+          }
+     }
+
+     res.json({ msg: 'Falta place holder' })
+}
+
+
 module.exports = {
      cargarArchivo,
-     actualizarImagen
+     actualizarImagen,
+     mostrarImage
 }
--- 07-restserver/routes/uploads.js
+++ 07-restserver/routes/uploads.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarCampos, validarArchivoSubir } = require('../middlewares');
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads.js');
+const { cargarArchivo, actualizarImagen, mostrarImage } = require('../controllers/uploads.js');
 const { coleccionesPermitidas } = require('../helpers');
 
 const router = Router();
@@ -15,6 +15,12 @@ router.put('/:coleccion/:id', [
      check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'] ) ),
      validarCampos
 ], actualizarImagen );
+
+router.get('/:coleccion/:id', [
+     check('id', 'El id debe de ser de mongo').isMongoId(),
+     check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'] ) ),
+     validarCampos
+], mostrarImage );
 
 
 module.exports = router;
