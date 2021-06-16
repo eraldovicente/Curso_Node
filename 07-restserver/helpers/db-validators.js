04e285e203c3f23bc05064568d6a16dd8dbf7dbe
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-16 16:01:55 -0300

    Ruta para actualizar imagenes de Usuarios y Prod.

--- 07-restserver/helpers/db-validators.js
+++ 07-restserver/helpers/db-validators.js
@@ -50,10 +50,23 @@ const existeUsuarioPorId = async( id ) => {
      }
 }
 
+/**
+ * Validar colecciones permitidas
+ */
+const coleccionesPermitidas = ( coleccion = '', colecciones = []) => {
+
+     const incluida = colecciones.includes( coleccion );
+     if ( !incluida ) {
+          throw new Error(`La colección ${ coleccion } no es permitida, ${ colecciones }`);
+     }
+     return true;
+}
+
 module.exports = {
      esRoleValido,
      emailExiste,
      existeUsuarioPorId,
      existeCategoriaPorId,
-     existeProductoPorId
+     existeProductoPorId,
+     coleccionesPermitidas
 }
