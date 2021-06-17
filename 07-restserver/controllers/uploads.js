5d7a46de67ea33b3c3371c5584ef847b211925a7
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-16 21:44:53 -0300

    Borrar imagenes de Cloudinary

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -108,7 +108,10 @@ const actualizarImagenCloudinary = async(req, res = response ) => {
 
      // Limpiar imágenes previas
      if ( modelo.img ) {
-         
+          const nombreArr     = modelo.img.split('/');
+          const nombre        = nombreArr[ nombreArr.length - 1 ];
+          const [ public_id ] = nombre.split('.');
+          cloudinary.uploader.destroy( public_id );
      }
 
      const { tempFilePath } = req.files.archivo;
@@ -117,7 +120,7 @@ const actualizarImagenCloudinary = async(req, res = response ) => {
 
      await modelo.save();
 
-     res.json( resp );
+     res.json( modelo );
 
 }
 
