4fd5ea92bce0a28f9d542c6ba982a3056470214f
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-17 22:03:26 -0300

    Mensajes de conexion y desconexion - Cliente

--- 08-websocket-server/public/js/socket-client.js
+++ 08-websocket-server/public/js/socket-client.js
@@ -1,2 +1,20 @@
 
+// Referencias de HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io();
+
+socket.on('connect', () => {
+     console.log('Conectado');
+
+     lblOffline.style.display = 'none';
+     lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+     console.log('Desconectado del servidor');
+
+     lblOffline.style.display = 'none';
+     lblOnline.style.display = '';
+});
