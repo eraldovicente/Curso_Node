e9fcd9f40254cd95fe6448d5e7dbc5fcee1de624
Eraldo Vicente <eraldovicente2@gmail.com>
2021-06-30 18:52:01 -0300

    Middlewares necessarios

--- 11-ts-restserver/models/server.ts
+++ 11-ts-restserver/models/server.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express';
 import userRoutes from '../routes/usuario';
+import cors from 'cors';
 
 class Server {
 
@@ -13,8 +14,23 @@ class Server {
           this.app = express();
           this.port = process.env.PORT || '8080';
 
-          // Definir mis rutas
+          // Métodos iniciales
+          this.middlewares();
           this.routes();
+     }
+
+     //TODO: Conectar base de datos
+
+     middlewares() {
+
+          // CORS
+          this.app.use( cors() );
+
+          // Lectura del body
+          this.app.use( express.json() );
+
+          // Carpeta pública
+          this.app.use( express.static('public') );
      }
 
      routes() {
